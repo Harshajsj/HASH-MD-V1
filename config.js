b2a62536af27546f1c546baa1dea357f2539cc1b
Hasmd02 <render2558@gmail.com>
2025-02-12 20:33:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,6 @@ module.exports = {
   OWNER_NUMBER: process.env.OWNER_NUMBER || "94727786760",
   //========================================- OTHER - CONFIGS -=====================================================================
   AUTO_VOICE: process.env.AUTO_VOICE || "true",
-  ANTI_BAD_WORDS_ENABLED: process.env.ANTI_BAD_WORDS_ENABLED || "true",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   ANTI_BAD_WORDS: (process.env.ANTI_BAD_WORDS || "pakayo,huththo").split(','),
   ANTI_LINK: process.env.ANTILINK || "true",
@@ -20,15 +19,11 @@ module.exports = {
   ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "true",
   ANTI_BOT: process.env.ANTI_BOT || "true",
   ANTI_DELETE: process.env.ANTI_DELETE || "true",
-  packname: process.env.packname || "SAHAS TECH",
-  author: process.env.author || "SAHAS TECH",
-  
   START_MSG: process.env.START_MSG || `HASH-MD Conected` ,
-
   ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || "Alive ban📍",
   MENU_IMG: process.env.MENU_IMG || "https://i.ibb.co/hRw1XK4/image.png",
   MENU_MSG: process.env.MENU_MSG || `Menumsg`,
-MENU_MS: process.env.MENU_MS || `menu 2`,
+  MENU_MS: process.env.MENU_MS || `menu 2`,
 
 };
