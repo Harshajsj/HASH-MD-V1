886f6e561462d68c4066893851f23864ab0f77bf
Hasmd02 <render2558@gmail.com>
2025-02-12 15:47:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,40 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "coQzmZYB#qntWaeFXb0NyYXxXE_EX9bUNR3__O377F2sZ2Fzc3s4",
+  SESSION_ID: process.env.SESSION_ID || "NzNg2DhC#npTlh-C0G7J4XJC7cKiX4Utl7KdKPqCK4IT3N9Mvk7s",
+  // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
+    // ADD Your MongoDB Database URL
   PREFIX: process.env.PREFIX || ".",
+  // Add Your Custom Prefix 
   mode: process.env.mode || "public",
+  // Add Your Bot Mode 
+  // private = Only Working For Owner Number
+  // public = AnyOne Working
+  // inbox = Only Working  Inbox
+  // groups = only working in group
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94727786760",
+  //========================================- OTHER - CONFIGS -=====================================================================
+  AUTO_VOICE: process.env.AUTO_VOICE || "true",
+  ANTI_BAD_WORDS_ENABLED: process.env.ANTI_BAD_WORDS_ENABLED || "true",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  ANTI_BAD_WORDS: (process.env.ANTI_BAD_WORDS || "pakayo,huththo").split(','),
+  ANTI_LINK: process.env.ANTILINK || "true",
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+  AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
+  ALWAYS_TYPING: process.env.ALWAYS_TYPING || "true",
+  ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "true",
+  ANTI_BOT: process.env.ANTI_BOT || "true",
+  ANTI_DELETE: process.env.ANTI_DELETE || "true",
+  packname: process.env.packname || "SAHAS TECH",
+  author: process.env.author || "SAHAS TECH",
+  //==========================================- API-CONFIGS -==========================================================
+  
+  START_MSG: process.env.START_MSG || `HASH-MD Conected` ,
+
   ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg",
   MENU_IMG: process.env.MENU_IMG || "https://i.ibb.co/hRw1XK4/image.png",
-  ALIVE_MSG: process.env.ALIVE_MSG || "i a alive now👀📍",
+  MENU_MSG: process.env.MENU_MSG || `Menumsg`,
+MENU_MS: process.env.MENU_MS || `menu 2`,
 
 };
