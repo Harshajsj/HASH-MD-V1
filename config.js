9464758f8650c7a14888f0f0bed1850c2a1656d1
Hasmd02 <render2558@gmail.com>
2025-02-12 15:32:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,28 +31,11 @@ module.exports = {
   packname: process.env.packname || "SAHAS TECH",
   author: process.env.author || "SAHAS TECH",
   //==========================================- API-CONFIGS -==========================================================
-  OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || "2d61a72574c11c4f36173b627f8cb177", //openweathermap.org
-  ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || "sk_6438bcc100d96458f8de0602aec662f4ba14b905fd090ad3", //elevenlabs.io
-  SHODAN_API: process.env.SHODAN_API || "cbCkidr6qd7AFVaYs56MuCouGfM8gFki", //developer.shodan.io
-  PEXELS_API_KEY: process.env.PEXELS_API_KEY || "39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH", // pexels.com
-  OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
-  PIXABAY_API_KEY: process.env.PIXABAY_API_KEY || "23378594-7bd620160396da6e8d2ed4d53", // pixabay.com
-  ZIPCODEBASE_API_KEY: process.env.ZIPCODEBASE_API_KEY || "0f94a5f0-6ea4-11ef-81da-579be4fb031c", // zipcodebase.com
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
-  GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
-  PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
-
-
-//------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------
-
   
-  START_MSG: process.env.START_MSG || `SAHAS-MD Conected` ,
-
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg",
-  MENU_IMG: process.env.MENU_IMG || "https://i.ibb.co/hRw1XK4/image.png",
-  MENU_MSG: process.env.MENU_MSG || `Menumsg`,
-    MENU_MS: process.env.MENU_MS || `menu 2`,
+START_MSG: process.env.START_MSG || `SHASH-MD Conected` ,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg",
+MENU_IMG: process.env.MENU_IMG || "https://i.ibb.co/hRw1XK4/image.png",
+MENU_MSG: process.env.MENU_MSG || `Menumsg`,
+MENU_MS: process.env.MENU_MS || `menu 2`,
 
 };
