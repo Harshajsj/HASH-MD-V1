cfd4386613c1ee27da3ef765bd61b3044e4d7f2f
Hasmd02 <render2558@gmail.com>
2025-02-12 14:37:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,11 +91,12 @@ async function connectToWA() {
 
     conn.ev.on('messages.upsert', async (mek) => {
         mek = mek.messages[0]
-        if (!mek.message) return
-        mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-        if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READE_STATUS === "true") {
-            await conn.readMessages([mek.key])
-        }
+if (!mek.message) return
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast' &&
+config.AUTO_READE_STATUS === "true") {
+await conn.readMessages([mek.key])
+}
         const m = sms(conn, mek)
         const type = getContentType(mek.message)
         const content = JSON.stringify(mek.message)
